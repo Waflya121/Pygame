f60d74d3a4755f0e784c8fc56b66c3b2cdc1a848
Waflya121 <lv.sumin@mail.ru>
2026-06-14 16:23:56 +0500

    Implement bullet shooting feature in main.py
    Added bullet shooting mechanics and handling in the game loop.

--- Звездные воители/main.py
+++ Звездные воители/main.py
@@ -11,6 +11,7 @@ pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
 player = Player()
+bullets = []
 
 # Главный цикл игры
 running = True
@@ -20,11 +21,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # Проверяем одиночное нажатие на Пробел
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                new_bullet = player.shoot()
+                bullets.append(new_bullet)
 
     player.update()
 
+    for bullet in bullets[:]:
+        bullet.update()
+        if bullet.y < -bullet.height:
+            bullets.remove(bullet)
+
     screen.fill(BLACK)
     player.draw(screen)
+
+    # Рисуем все пули
+    for bullet in bullets:
+        bullet.draw(screen)
 
     pygame.display.flip()
 
