beee67c4fa94c02e4749dad47e34b0cf756bdaaa
Waflya121 <lv.sumin@mail.ru>
2026-06-15 12:47:03 +0500

    Update enemy.py

--- Звездные воители/enemy.py
+++ Звездные воители/enemy.py
@@ -1,6 +1,6 @@
-# enemy.py
 import pygame
-from config import HEIGHT, ENEMY_SPEED, RED, ENEMY_WIDTH, ENEMY_HEIGHT
+from config import HEIGHT, ENEMY_SPEED, RED, ENEMY_WIDTH, ENEMY_HEIGHT, ENEMY_RETURN_SPEED, ENEMY_SHOOT_CHANCE
+from bullet import EnemyBullet
 
 class Enemy:
     def __init__(self, start_x, target_y):
@@ -13,27 +13,35 @@ class Enemy:
         
         self.current_dive_x = 0
         self.speed = ENEMY_SPEED
+        self.return_speed = ENEMY_RETURN_SPEED
         self.is_attacking = False
         self.is_returning = False
         self.color = RED
+
+    def shoot(self):
+        if self.is_attacking and random.randint(0, ENEMY_SHOOT_CHANCE) == 1:
+            return EnemyBullet(self.x + self.width // 2, self.y + self.height)
+        return None
 
     def update(self):
         if self.is_attacking:
             self.y += self.speed
             self.x += self.current_dive_x
-            
             if self.y > HEIGHT:
                 self.y = -self.height
                 self.is_attacking = False
                 self.is_returning = True
 
         elif self.is_returning:
-            if abs(self.x - self.original_x) > 2:
-                if self.x < self.original_x: self.x += 3
-                else: self.x -= 3
+            if self.y < 0:
+                if abs(self.x - self.original_x) > 3:
+                    if self.x < self.original_x: self.x += 6 
+                    else: self.x -= 6
+                else:
+                    self.x = self.original_x
+                    self.y += self.return_speed
             else:
-                self.x = self.original_x
-                self.y += self.speed
+                self.y += self.return_speed
             
             if self.y >= self.target_y:
                 self.y = self.target_y
